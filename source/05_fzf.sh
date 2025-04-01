bcba27d27908fd6825f7a1651b14319afb1923d7
Ryan Ulep <ryanulep@uber.com>
2025-04-01 13:29:41 +0200

    Fix styling of output in fzf.sh

--- source/05_fzf.sh
+++ source/05_fzf.sh
@@ -10,22 +10,20 @@ if [[ ! -z "$DEVPOD_NAME" ]]; then
 fi
 
 ## Fzf (The following setup is from https://github.com/junegunn/fzf)
-export FZF_DEFAULT_OPTS=""
+export FZF_DEFAULT_OPTS="--height=100% --style full"
 # Setting fd as the default source for fzf; add `--hidden`` so that hidden files are included below
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --follow --exclude .git'
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
-  --bind="ctrl-o:execute(code {})+abort"'
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'--bind="ctrl-o:execute(code {})+abort"'
+
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="
-  --height=80%
-  --style full
   --walker-skip .git,node_modules,target
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
 # CTRL-Y to copy the command into clipboard using pbcopy
 export FZF_CTRL_R_OPTS="
-  --style full
   --multi
   --height=60%
   --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
