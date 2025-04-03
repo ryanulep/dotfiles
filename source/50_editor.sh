b6be0688cae53802377e40c521e0aff7c8763bda
Ryan Ulep <ryanulep@uber.com>
2025-04-03 10:19:04 +0200

    Prevent Homebrew analytics and enable colors

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -5,5 +5,12 @@ export VISUAL="$EDITOR"
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
 
+# Makes color constants available
+autoload -U colors
+colors
+
 ## Bat (https://github.com/sharkdp/bat)
 export BAT_CONFIG_PATH="$HOME/.config/bat/bat.conf"
+
+# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
+export HOMEBREW_NO_ANALYTICS=1
