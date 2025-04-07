3ee8a99664dd4ca61a81f45d51e0ca56cf605c31
Ryan Ulep <ryanulep@uber.com>
2025-04-07 15:57:57 +0200

    Temp disable manual setup to see if zsh plugin handles this

--- source/05_fzf.sh
+++ source/05_fzf.sh
@@ -27,13 +27,13 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 
-if [[ ! -z "$DEVPOD_NAME" ]]; then
-    # Install latest version on devpod
-    if [[ ! -f "$HOME/.fzf.zsh" ]]; then
-        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-        echo "y y n " | tr ' ' '\n' | ~/.fzf/install
-    fi
-    export FZF_PATH="$HOME/.fzf"
-    export PATH="$HOME/.fzf/bin:$PATH"
-    source "$HOME/.fzf.zsh"
-fi
+# if [[ ! -z "$DEVPOD_NAME" ]]; then
+#     # Install latest version on devpod
+#     if [[ ! -f "$HOME/.fzf.zsh" ]]; then
+#         git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#         echo "y y n " | tr ' ' '\n' | ~/.fzf/install
+#     fi
+#     export FZF_PATH="$HOME/.fzf"
+#     export PATH="$HOME/.fzf/bin:$PATH"
+#     source "$HOME/.fzf.zsh"
+# fi
