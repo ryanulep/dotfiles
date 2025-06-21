8ff392f239ddfe0b5305c255c8685ef12d12b05c
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2025-06-21 22:19:11 +0200

    Re-order setup and download additional binaries

--- source/20_plugins.sh
+++ source/20_plugins.sh
@@ -27,12 +27,11 @@ if ! zgenom saved; then
   zgenom load unixorn/fzf-zsh-plugin
   zgenom ohmyzsh plugins/fzf
   zgenom ohmyzsh plugins/command-not-found  # Provide suggested packages to be installed if a command cannot be found
-  zgenom ohmyzsh plugins/bazel
   zgenom load zsh-users/zsh-autosuggestions
   zgenom load zsh-users/zsh-completions
 
   # Production environment management
-  zgenom ohmyzsh plugins/docker  # auto-completion and aliases for Docker
+  zgenom ohmyzsh plugins/docker  # Auto-completion and aliases for Docker
   zgenom ohmyzsh plugins/docker-compose
   zgenom ohmyzsh plugins/kubectl
   zgenom ohmyzsh plugins/kubectx
@@ -48,45 +47,49 @@ if ! zgenom saved; then
   zgenom ohmyzsh plugins/tig
   zgenom load so-fancy/diff-so-fancy
 
-  # System enhancements
-  zgenom ohmyzsh plugins/iterm2
-  zgenom ohmyzsh plugins/tmux
+  # File management
+  zgenom load raisedadead/zsh-touchplus  # create files with touch including the path
   zgenom ohmyzsh plugins/wd  # Jump to custom directories in zsh without using `cd`
   zgenom ohmyzsh plugins/extract  # Extract compressed files
+  zgenom ohmyzsh plugins/colorize  # Provides syntax highlighting for files
+  zgenom ohmyzsh plugins/zoxide  # Smarter cd command
+
+  # Shell enhancements
+  zgenom ohmyzsh plugins/iterm2
+  zgenom ohmyzsh plugins/tmux
+  zgenom ohmyzsh plugins/thefuck  # Corrects mistyped commands
+  zgenom load zsh-users/zsh-syntax-highlighting  # Provides CLI syntax highlighting
   zgenom ohmyzsh plugins/aliases  # Lists shortcuts available based on installed plugins
   zgenom ohmyzsh plugins/alias-finder  # Searches defined aliases and outputs any that match the command inputted
-  zgenom ohmyzsh plugins/colorize  # Provides syntax highlighting for files
-  zgenom load zsh-users/zsh-syntax-highlighting  # Provides CLI syntax highlighting
-  zgenom ohmyzsh plugins/zoxide  # Smarter cd command
   zgenom ohmyzsh plugins/safe-paste  # Review what was actually pasted before running it
   zgenom ohmyzsh plugins/last-working-dir  # Track the last used working directory and automatically jump into it for new shells
   zgenom ohmyzsh plugins/common-aliases  # Creates helpful shortcut aliases for many commonly used commands
   zgenom ohmyzsh plugins/zsh-interactive-cd  # Interactive way to change directories in zsh using fzf
-
-  # Productivity
-  zgenom ohmyzsh plugins/web-search  # Search the web from the command line
-
-  # Random Extras
-  zgenom load djui/alias-tips  # Warns you when you have an alias for the command you just typed
-  zgenom ohmyzsh plugins/jsontools  # Handling JSON data
-  zgenom ohmyzsh plugins/colored-man-pages  # Colored man pages
   zgenom ohmyzsh plugins/copyfile
   zgenom ohmyzsh plugins/copybuffer  # Copy the contents of buffer to the clipboard using Ctrl + "O"
   zgenom ohmyzsh plugins/copypath
+  zgenom load djui/alias-tips  # Warns you when you have an alias for the command you just typed
+  zgenom ohmyzsh plugins/colored-man-pages  # Colored man pages
   zgenom ohmyzsh plugins/timer  # Timer plugin to measure the time it takes to run a command
 
-  zgenom load jandamm/zgenom-ext-eval
-  zgenom load jandamm/zgenom-ext-release  # Download releases from GitHub
-  zgenom load jandamm/zgenom-ext-run
+  # Productivity
+  zgenom ohmyzsh plugins/web-search  # Search the web from the command line
+  zgenom ohmyzsh plugins/bazel  # Bazel build system support
+  zgenom ohmyzsh plugins/jsontools  # Handling JSON data
+  
+  # Core Zsh plugins
+  zgenom load jandamm/zgenom-ext-eval  # Quickly generate plugins from a command or heredoc.
+  zgenom load jandamm/zgenom-ext-release  # Use zgenom and gh to download github releases.
+  zgenom load jandamm/zgenom-ext-run  # Run commands in the plugin folder
   zgenom load qoomon/zsh-lazyload
+  zgenom load aloxaf/fzf-tab
 
   # Install macOS only plugins, if needed
   [[ "$(uname -s)" = Darwin ]] && zgenom ohmyzsh plugins/macos
   [[ "$(uname -s)" = Darwin ]] && zgenom ohmyzsh plugins/vscode
   [[ "$(uname -s)" = Darwin ]] && zgenom ohmyzsh plugins/brew
   [[ "$(uname -s)" = Darwin ]] && zgenom ohmyzsh plugins/xcode
-
-  zgenom load aloxaf/fzf-tab
+  [[ "$(uname -s)" = Darwin ]] && zgenom load nilsonholger/osx-zsh-completions
 
   # Add binaries
   zgenom bin tj/git-extras
@@ -98,14 +101,20 @@ if ! zgenom saved; then
   # Install core apps using eget
   command -v zoxide > /dev/null 2>&1 || eget ajeetdsouza/zoxide
   command -v bat > /dev/null 2>&1 || eget sharkdp/bat
-  command -v ripgrep > /dev/null 2>&1 | eget BurntSushi/ripgrep
-  command -v fd > /dev/null 2>&1 | eget sharkdp/fd
-  # command -v ranger > /dev/null 2>&1 | eget ranger/ranger ## Currently not working with zgenom
+  command -v direnv > /dev/null 2>&1 || eget direnv/direnv
+  command -v rg > /dev/null 2>&1 || eget BurntSushi/ripgrep
+  command -v fd > /dev/null 2>&1 || eget sharkdp/fd
+  command -v autoenv > /dev/null 2>&1 || eget hyperupcall/autoenv
+
+  # Install plugins required after the core apps are installed
+  zgenom ohmyzsh plugins/direnv  # Load and unload environment variables per directory
+  zgenom ohmyzsh plugins/autoenv  # Automatically load and unload environment variables per directory
 
   lazyload sdk -- 'export SDKMAN_DIR="$HOME/.sdkman" && source "$HOME/.sdkman/bin/sdkman-init.sh"'
   lazyload nvm npm node -- 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
 
-  zgenom eval --name zoxide <<(zoxide init zsh)
+  zgenom eval --name zoxide <<(zoxide init zsh --cmd cd)
+  zgenom eval --name wtf $(thefuck --alias wtf)
 
   # save all to init script
   zgenom save
