e711d402e0c9b0388c619571286a97065359e15c
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2025-06-22 17:12:37 +0200

    Add support for delta and the customized configuration

--- source/61_envrc.sh
+++ source/61_envrc.sh
@@ -3,3 +3,6 @@ export BAT_CONFIG_PATH="$HOME/.config/bat/config"
 
 # To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
 export HOMEBREW_NO_ANALYTICS=1
+
+# If delta is installed, use delta by default.
+BATDIFF_USE_DELTA=true
