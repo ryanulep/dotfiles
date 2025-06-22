d8468d3acd28730fe7f03c9731dbbf8828f020fc
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2025-06-22 15:20:04 +0200

    Fix bat color scheme for both light and dark mode

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -1,5 +1,5 @@
 ## For a full list of active aliases, run `alias`.
-alias cat='bat --theme auto:system --style plain --paging=never'
+alias cat='bat --style plain --paging=never'
 
 # Fzf
 alias fzf="fzf --style full --preview 'bat -n --theme auto:system --color=always {}' --bind 'focus:transform-header:file --brief {}'"
