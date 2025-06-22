537e2572a1e9c9a4c941befa39665a9d25506dd6
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2025-06-22 14:59:49 +0200

    Move some environment variables to 61_envrc

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -8,9 +8,3 @@ export CLICOLOR=1
 # Makes color constants available
 autoload -U colors
 colors
-
-## Bat (https://github.com/sharkdp/bat)
-export BAT_CONFIG_PATH="$HOME/.config/bat/config"
-
-# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
-export HOMEBREW_NO_ANALYTICS=1
---
+++ source/61_envrc.sh
@@ -0,0 +1,5 @@
+## Bat (https://github.com/sharkdp/bat)
+export BAT_CONFIG_PATH="$HOME/.config/bat/config"
+
+# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
+export HOMEBREW_NO_ANALYTICS=1
