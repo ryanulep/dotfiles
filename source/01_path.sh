6f3b82ce3130a6b72b6212d2cbec9ab2e622165b
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2025-06-22 12:12:29 +0200

    Update 01_path.sh to append new PATHs at the end of the existing PATH variable

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,5 +1,6 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$DOTFILES/bin:$HOME/bin:/usr/local/sbin:$PATH"
-PATH="/opt/homebrew/bin:$PATH"
-PATH="/opt/homebrew/sbin:$PATH"
+PATH="$PATH:/opt/homebrew/bin"
+PATH="$PATH:/opt/homebrew/sbin"
+
 export -U PATH
