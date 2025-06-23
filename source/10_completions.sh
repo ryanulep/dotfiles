6c282619a0dfa4df631e8e88239f3e8c0b15e88d
Ryan Ulep <ryanulep@uber.com>
2025-06-23 13:27:17 +0200

    Add Docker completions to fpath

--- source/10_completions.sh
+++ source/10_completions.sh
@@ -1,7 +1,9 @@
-# load our own completion functions
+# Load our own completion functions
 fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=($HOME/.docker/completions $fpath)
 
-# completion; use cache if updated within 24h
+# Completion; use cache if updated within 24h
 autoload -Uz compinit
 if [[ -n $HOME/.zcompdump(#qN.mh+24) ]]; then
   compinit -d $HOME/.zcompdump;
