40152c7e5844d791094b27eb882298e04fd9a4d7
Ryan Ulep <ryanulep@uber.com>
2025-06-25 10:33:56 +0200

    Add TIMER_THRESHOLD to only output for long-running commands

--- source/61_envrc.sh
+++ source/61_envrc.sh
@@ -3,6 +3,8 @@ export BAT_CONFIG_PATH="$HOME/.config/bat/config"
 # If delta is installed, use delta by default.
 BATDIFF_USE_DELTA=true
 
+export TIMER_THRESHOLD=2
+
 # To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
 export HOMEBREW_NO_ANALYTICS=1
 
