adc689088d236f33620716a36ab4dc07d2fabe75
Ryan Ulep <ryanulep@uber.com>
2025-10-16 14:51:09 +0200

    Add lg as alias for lazygit only if installed

--- source/60_aliases.sh
+++ source/60_aliases.sh
@@ -1,6 +1,11 @@
 ## For a full list of active aliases, run `alias`.
 command -v bat >/dev/null 2>&1 && alias cat='bat --style plain --paging=never'
 
+# Alias lg -> lazygit only if it's installed
+if (( $+commands[lazygit] )); then
+  alias lg='lazygit'
+fi
+
 # Include custom aliases
 if [[ -f ~/.aliases.local ]]; then
 	source ~/.aliases.local
