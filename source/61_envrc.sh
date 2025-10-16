4e587109752da74a774ce393aad043cfb1ea687d
Ryan Ulep <ryanulep@uber.com>
2025-10-16 16:36:11 +0200

    Add configuration for lazygit
    GH-27

--- source/61_envrc.sh
+++ source/61_envrc.sh
@@ -3,6 +3,11 @@ export BAT_CONFIG_PATH="$HOME/.config/bat/config"
 # If delta is installed, use delta by default.
 BATDIFF_USE_DELTA=true
 
+# Export XDG_CONFIG_HOME -> ~/.config only if it's not already set
+if (( ! ${+XDG_CONFIG_HOME} )); then
+  export XDG_CONFIG_HOME="$HOME/.config"
+fi
+
 export TIMER_THRESHOLD=2
 
 # To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
