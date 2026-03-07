0cc7ddccb685447eae4101f4aa7e5a66e381048f
Ryan Ulep <11778394+ryanulep@users.noreply.github.com>
2026-03-07 22:56:45 +0100

    Update HISTORY settings

--- source/50_history.sh
+++ source/50_history.sh
@@ -1,9 +1,32 @@
-setopt hist_ignore_all_dups inc_append_history
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_reduce_blanks
+setopt hist_verify
+setopt hist_ignore_space
+setopt inc_append_history
 setopt share_history
+setopt hist_find_no_dups
+setopt hist_no_store
+setopt hist_fcntl_lock
+setopt extended_history
+
 HISTFILE=~/.zsh_history
-HISTORY_IGNORE='(l|ls|ll|cd|cd ..|z|pwd|exit|date|history|clear|tmux|tmux attach)'
 HISTSIZE=290000
 SAVEHIST=$HISTSIZE
+
+HISTORY_IGNORE='
+  (l|ls|ll|la)( * )|
+  (cd|z)( * )|
+  pwd|exit|date|
+  history( * )|
+  clear|reset|
+  tmux( * )|
+  (lg|lazygit|tig)|
+  git (status|diff|log)( * )|
+  jj (st|status|diff|log|show|obslog|evolog)( * )|
+  jj bookmark list( * )|
+  jj git remote list( * )
+'
 
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_USE_ASYNC=1
